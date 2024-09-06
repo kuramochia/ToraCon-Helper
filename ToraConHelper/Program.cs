6b04c3b8b79a90cfbd6b04dab5421c9dbb341df6
kuramochia <kuramochia@live.jp>
2024-09-06 16:02:34 +0900

    多重起動防止を実装

---
+++ ToraConHelper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace ToraConHelper;
+
+public class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        // 多重起動防止用 Mutex
+        using Mutex mutex = new(true, "ToraConHelper", out var createdNew);
+        try
+        {
+            if (createdNew)
+            {
+                // 新規起動
+                var app = new App();
+                app.Run();
+            }
+        }
+        finally
+        {
+            if (createdNew) mutex.ReleaseMutex();
+        }
+    }
+}
