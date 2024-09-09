b17ccaddb2bd28de64c7e4eb11716db145cc3be9
kuramochia <kuramochia@live.jp>
2024-09-09 19:52:13 +0900

    WPF-UI への参照に合わせてUI実装を変更

---
+++ ToraConHelper/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using ToraConHelper.Views;
+using Wpf.Ui.Controls;
+
+namespace ToraConHelper.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<INavigationViewItem>? mainNavigationViewItems;
+    [ObservableProperty]
+    private ObservableCollection<INavigationViewItem>? footerNavigationViewItems;
+
+    public MainWindowViewModel()
+    {
+        MainNavigationViewItems =
+            [
+                new NavigationViewItem{
+                    Content="Home",
+                    Icon = new SymbolIcon{ Symbol = SymbolRegular.Home24 },
+                    TargetPageType = typeof(HomePage),
+                    TargetPageTag = nameof(HomePage),
+                },
+            ];
+
+        FooterNavigationViewItems =
+            [
+                new NavigationViewItem{
+                    Content = "Abount",
+                    Icon = new SymbolIcon { Symbol = SymbolRegular.Settings24 },
+                    TargetPageType= typeof(AboutPage),
+                    TargetPageTag= nameof(AboutPage),
+                },
+            ];
+    }
+}
