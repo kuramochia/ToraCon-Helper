3bce6200f59796b7f7d5039851a893024d18948e
kuramochia <kuramochia@live.jp>
2024-09-13 18:08:42 +0900

    ViewModel分割とSettingsの整形

---
+++ ToraConHelper/ViewModels/ViewModel.Blinker.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using ToraConHelper.Services.TelemetryActions;
+
+namespace ToraConHelper.ViewModels;
+
+public partial class ViewModel
+{
+    [ObservableProperty]
+    private bool blinkerLikeRealCarActionEnabled;
+
+    partial void OnBlinkerLikeRealCarActionEnabledChanged(bool oldValue, bool newValue) => OnActionEnabledChanged<BlinkerLikeRealCarAction>(oldValue, newValue);
+
+    [ObservableProperty]
+    private bool blinkerHideOnSteeringActionEnabled;
+
+    partial void OnBlinkerHideOnSteeringActionEnabledChanged(bool oldValue, bool newValue) => OnActionEnabledChanged<BlinkerHideOnSteeringAction>(oldValue, newValue);
+
+    // ハンドル回転角
+    [ObservableProperty]
+    private int steeringRotationAngle;
+
+    partial void OnSteeringRotationAngleChanged(int value) => SteeringAngleChanged();
+    // ウィンカー消す角度
+    [ObservableProperty]
+    private int blinkerHideBySteeringAngle;
+
+    partial void OnBlinkerHideBySteeringAngleChanged(int newValue) => SteeringAngleChanged();
+
+    private void SteeringAngleChanged()
+    {
+        var action = App.Current.Services.GetService<BlinkerHideOnSteeringAction>();
+        // 角度から相対値に変換
+        action!.BlinkerHidePosition = (float)BlinkerHideBySteeringAngle / (SteeringRotationAngle / 2);
+    }
+}
---
+++ ToraConHelper/ViewModels/ViewModel.Retarder.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using ToraConHelper.Services.TelemetryActions;
+
+namespace ToraConHelper.ViewModels;
+
+public partial class ViewModel
+{
+    [ObservableProperty]
+    private bool reterderAllReduceActionEnabled;
+
+    partial void OnReterderAllReduceActionEnabledChanged(bool oldValue, bool newValue) => OnActionEnabledChanged<ReterderAllReduceAction>(oldValue, newValue);
+
+    [ObservableProperty]
+    private int reterderAllReduceActionLimitSpeedKph;
+
+    partial void OnReterderAllReduceActionLimitSpeedKphChanged(int newValue)
+    {
+        var action = App.Current.Services.GetService<ReterderAllReduceAction>();
+        action!.LimitSpeedKph = newValue;
+    }
+
+    // リターダーを一段入れたら最大段数まで上げる
+    [ObservableProperty]
+    private bool retarderFullOnActionEnabled;
+
+    partial void OnRetarderFullOnActionEnabledChanged(bool oldValue, bool newValue) => OnActionEnabledChanged<RetarderFullOnAction>(oldValue, newValue);
+
+
+    // リターダーをフルから一段下げたら 0 段にする
+    [ObservableProperty]
+    private bool retarderFullOffActionEnabled;
+
+    partial void OnRetarderFullOffActionEnabledChanged(bool oldValue, bool newValue) => OnActionEnabledChanged<RetarderFullOffAction>(oldValue, newValue);
+}
