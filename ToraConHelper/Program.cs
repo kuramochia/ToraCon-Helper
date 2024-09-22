6c60ec97564f0fef3555b98c309acc0f51859e32
kuramochia <kuramochia@live.jp>
2024-09-22 20:10:03 +0900

    テレメトリ自動インストール機能

--- ToraConHelper/Program.cs
+++ ToraConHelper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using ToraConHelper.Installer;
 
 namespace ToraConHelper;
 
@@ -8,20 +9,28 @@ public class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        // 多重起動防止用 Mutex
-        using Mutex mutex = new(true, "ToraConHelper", out var createdNew);
-        try
+        if ( args.Length == 1 && string.Equals("-install", args[0],StringComparison.OrdinalIgnoreCase))
         {
-            if (createdNew)
-            {
-                // 新規起動
-                var app = new App();
-                app.Run();
-            }
+            var pluginApp = new PluginApp();
+            pluginApp.Run();
         }
-        finally
+        else
         {
-            if (createdNew) mutex.ReleaseMutex();
+            // 多重起動防止用 Mutex
+            using Mutex mutex = new(true, "ToraConHelper", out var createdNew);
+            try
+            {
+                if (createdNew)
+                {
+                    // 新規起動
+                    var app = new App();
+                    app.Run();
+                }
+            }
+            finally
+            {
+                if (createdNew) mutex.ReleaseMutex();
+            }
         }
     }
 }
