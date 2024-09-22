006ea85fd93ae22b690ff82bd49d0456d3043c2d
kuramochia <kuramochia@live.jp>
2024-09-22 13:39:24 +0900

    給油を開始すると、ボタンを離しても満タンまで給油し続ける アクション追加

---
+++ ToraConHelper/Services/TelemetryActions/AutoFullFuelAction.cs
@@ -0,0 +1,54 @@
+﻿using SCSSdkClient.Input;
+using SCSSdkClient.Object;
+using System.Diagnostics;
+
+namespace ToraConHelper.Services.TelemetryActions;
+
+internal class AutoFullfuelAction : ITelemetryActionWithEvents
+{
+    public void OnTelemetryUpdated(SCSTelemetry telemetry)
+    {
+    }
+
+    public void OnRefuelStart()
+    {
+        //Debug.WriteLine(nameof(OnRefuelStart));
+        SetActivate(true);
+    }
+
+    public void OnRefuelEnd()
+    {
+        //Debug.WriteLine(nameof(OnRefuelEnd));
+        SetActivate(false);
+    }
+
+    public void OnRefuelPayed()
+    {
+        //Debug.WriteLine(nameof(OnRefuelPayed));
+        SetActivate(false);
+    }
+
+    private void SetActivate(bool value)
+    {
+        //Debug.WriteLine($"{nameof(SetActivate)}({value})");
+        using var input = new SCSSdkTelemetryInput();
+        input.Connect();
+        input.SetActivate(value);
+    }
+
+
+    #region Unused events
+    public void OnFerry() { }
+
+    public void OnFined() { }
+
+    public void OnJobCancelled() { }
+
+    public void OnJobDelivered() { }
+
+    public void OnJobStarted() { }
+    public void OnTollgate() { }
+
+    public void OnTrain() { }
+    #endregion
+}
