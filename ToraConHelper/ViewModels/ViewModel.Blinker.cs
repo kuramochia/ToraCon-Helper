6bae6a2e00c06c1fab196da6064778091c8606fe
kuramochia <kuramochia@live.jp>
2024-09-23 15:19:16 +0900

    高速道路 車線変更用ショート ウィンカー アクション実装

--- ToraConHelper/ViewModels/ViewModel.Blinker.cs
+++ ToraConHelper/ViewModels/ViewModel.Blinker.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Gameloop.Vdf.Linq;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using ToraConHelper.Services.TelemetryActions;
 
 namespace ToraConHelper.ViewModels;
@@ -20,17 +22,65 @@ public partial class ViewModel
     [ObservableProperty]
     private int steeringRotationAngle;
 
-    partial void OnSteeringRotationAngleChanged(int value) => SteeringAngleChanged();
+    partial void OnSteeringRotationAngleChanged(int value)
+    {
+        BlinkerHideBySteeringAngleChanged();
+        BlinkerForLaneChangeSteeringAngleChanged();
+    }
+
     // ウィンカー消す角度
     [ObservableProperty]
     private int blinkerHideBySteeringAngle;
 
-    partial void OnBlinkerHideBySteeringAngleChanged(int newValue) => SteeringAngleChanged();
+    partial void OnBlinkerHideBySteeringAngleChanged(int newValue) => BlinkerHideBySteeringAngleChanged();
 
-    private void SteeringAngleChanged()
+    private void BlinkerHideBySteeringAngleChanged()
     {
         var action = App.Current.Services.GetService<BlinkerHideOnSteeringAction>();
         // 角度から相対値に変換
         action!.BlinkerHidePosition = (float)BlinkerHideBySteeringAngle / (SteeringRotationAngle / 2);
     }
+
+    // 車線変更ウィンカーアクション
+    [ObservableProperty]
+    private bool blinkerForLaneChangeEnabled;
+
+    partial void OnBlinkerForLaneChangeEnabledChanged(bool oldValue, bool newValue) => OnActionEnabledChanged<BlinkerForLaneChangeAction>(oldValue, newValue);
+
+    // 車線変更ウィンカー監視開始速度
+    [ObservableProperty]
+    private int blinkerForLaneChangeLimitSpeedKph;
+
+    partial void OnBlinkerForLaneChangeLimitSpeedKphChanged(int value)
+    {
+        var action = App.Current.Services.GetService<BlinkerForLaneChangeAction>();
+        action!.LowerLimitKph = value;
+    }
+
+    // 車線変更ウィンカー監視範囲ステアリング角度
+    [ObservableProperty]
+    private int blinkerForLaneChangeSteeringAngle;
+
+    partial void OnBlinkerForLaneChangeSteeringAngleChanged(int value)
+    {
+        BlinkerForLaneChangeSteeringAngleChanged();
+    }
+
+    private void BlinkerForLaneChangeSteeringAngleChanged()
+    {
+        var action = App.Current.Services.GetService<BlinkerForLaneChangeAction>();
+        // 角度から相対値に変換
+        action!.SteeringLimit = (float)BlinkerForLaneChangeSteeringAngle / (SteeringRotationAngle / 2);
+    }
+
+    // 車線変更ウィンカーオフまでの時間(秒)
+    [ObservableProperty]
+    private int blinkerForLaneChangeOffSeconds;
+
+    partial void OnBlinkerForLaneChangeOffSecondsChanged(int value)
+    {
+        var action = App.Current.Services.GetService<BlinkerForLaneChangeAction>();
+        action!.OffTime = TimeSpan.FromSeconds(value);
+    }
+
 }
