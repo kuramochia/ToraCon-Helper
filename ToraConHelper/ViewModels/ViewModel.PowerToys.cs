a993d4b30d311ab6c4968802cec3f7a25e665590
kuramochia <kuramochia@live.jp>
2024-10-13 20:22:35 +0900

    PowerToys 新規作成

---
+++ ToraConHelper/ViewModels/ViewModel.PowerToys.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToraConHelper.ViewModels;
+
+public partial class ViewModel
+{
+	[ObservableProperty]
+	private PowerToysViewModel? ets2;
+
+	[ObservableProperty]
+	private PowerToysViewModel? ats;
+}
