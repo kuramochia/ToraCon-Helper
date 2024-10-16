97af4f603beb897531ba85e0a17a91499cd1110c
kuramochia <kuramochia@live.jp>
2024-10-16 10:11:54 +0900

    プロセス多重起動時にウィンドウを表示する

--- ToraConHelper/App.xaml.cs
+++ ToraConHelper/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Diagnostics;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using ToraConHelper.Installer;
 using ToraConHelper.Services;
@@ -16,6 +19,8 @@ namespace ToraConHelper;
 /// </summary>
 public partial class App : Application
 {
+    internal const string NamedPipeName = "ToraConHelper-NamedPipe-4-MultiInstance";
+
     public App() : base()
     {
         Services = ConfigureServices();
@@ -27,12 +32,15 @@ public partial class App : Application
     private EventHandler? showAction;
 
     private EventHandler? showPowerToysAction;
+
+    private CancellationTokenSource cancellationTokenSource = new();
+
     public new static App Current => (App)Application.Current;
 
     public IServiceProvider Services { get; }
 
 
-    protected override void OnStartup(StartupEventArgs e)
+    protected override async void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
         // ちょっと雑だけど、MainWindowを DI から取る -> ViewModel を DI から取る → 初期化と動作開始、という流れ
@@ -72,6 +80,8 @@ public partial class App : Application
 
         // Telemetry DLL 更新チェック
         CheckTelemetryDLL();
+
+        await RunPerProcessCommunicationAsync(cancellationTokenSource.Token).ConfigureAwait(false);
     }
 
     private void CheckTelemetryDLL()
@@ -92,6 +102,8 @@ public partial class App : Application
 
     protected override void OnExit(ExitEventArgs e)
     {
+        cancellationTokenSource.Cancel();
+        cancellationTokenSource.Dispose();
         // Singleton Cleanup
         (Services as IDisposable)?.Dispose();
         notifyIcon!.DoubleClick -= showAction;
@@ -131,5 +143,23 @@ public partial class App : Application
         services.AddSingleton<BlinkerLikeRealCarDInputAction>();
 
         return services.BuildServiceProvider();
+    }
+
+    private async Task RunPerProcessCommunicationAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                using var server = new NamedPipeServerStream(NamedPipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+                await server.WaitForConnectionAsync(cancellationToken);
+                // 特別な通信内容はいらないので、接続されたら Show() の依頼、ということにする
+                await Dispatcher.InvokeAsync(() => showAction?.Invoke(this, EventArgs.Empty));
+            }
+            catch
+            {
+                continue;
+            }
+        }
     }
 }
