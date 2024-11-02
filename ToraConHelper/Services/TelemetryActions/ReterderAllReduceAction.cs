2cb735f56c51d9d3b6efd172cff69e2d4773bf44
kuramochia <kuramochia@live.jp>
2024-11-02 11:40:00 +0900

    指定速度以下でリターダーを全部戻す アクションがタイミングにより正しく動作しない問題の対応

--- ToraConHelper/Services/TelemetryActions/ReterderAllReduceAction.cs
+++ ToraConHelper/Services/TelemetryActions/ReterderAllReduceAction.cs
@@ -6,34 +6,29 @@ namespace ToraConHelper.Services.TelemetryActions;
 public class ReterderAllReduceAction : TelemetryActionBase
 {
     /// <summary>
-    /// リターダーを前段戻す速度
+    /// リターダーを全段戻す速度
     /// </summary>
     public int LimitSpeedKph { get; set; } = 30;
 
-    private int _reterderLevel = 0;
+    private uint _reterderLevel = 0;
 
     public override void OnTelemetryUpdated(SCSTelemetry telemetry)
     {
         var reterderLevel = telemetry.TruckValues.CurrentValues.MotorValues.BrakeValues.RetarderLevel;
-        // リターダー段数が、前と変わらないか、前よりも大きい場合
-        if (_reterderLevel <= reterderLevel)
+        var currentSpeedKph = telemetry.TruckValues.CurrentValues.DashboardValues.Speed.Kph;
+        // リターダー段数を減らしてる
+        // 速度が制限速度以下の場合
+        if (_reterderLevel > 0 && _reterderLevel > reterderLevel && currentSpeedKph <= LimitSpeedKph)
         {
-            // 値を取得
-            _reterderLevel = (int)reterderLevel;
+            // リターダー段数0を入力する
+            using var input = new SCSSdkTelemetryInput();
+            input.Connect();
+            input.SetRetarder(0);
+            _reterderLevel = 0;
         }
-        else if (reterderLevel > 0)
+        else
         {
-            // リターダー段数を減らしてる
-            var currentSpeedKph = telemetry.TruckValues.CurrentValues.DashboardValues.Speed.Kph;
-            // 速度が制限速度以下の場合
-            if (currentSpeedKph <= LimitSpeedKph)
-            {
-                // リターダー段数0を入力する
-                using var input = new SCSSdkTelemetryInput();
-                input.Connect();
-                input.SetRetarder(0);
-                _reterderLevel = 0;
-            }
+            _reterderLevel = reterderLevel;
         }
     }
 }
