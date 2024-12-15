ffb859f503fc6450d6d2c48dc83df08de50fd6c7
kuramochia <kuramochia@live.jp>
2024-12-15 17:51:16 +0900

    fix more

--- ToraConHelper/Services/TelemetryActions/ReterderAllReduceAction.cs
+++ ToraConHelper/Services/TelemetryActions/ReterderAllReduceAction.cs
@@ -14,6 +14,7 @@ public class ReterderAllReduceAction : TelemetryActionBase
 
     public override bool OnTelemetryUpdated(SCSTelemetry telemetry)
     {
+        var changed = false;
         var reterderLevel = telemetry.TruckValues.CurrentValues.MotorValues.BrakeValues.RetarderLevel;
         var currentSpeedKph = telemetry.TruckValues.CurrentValues.DashboardValues.Speed.Kph;
         // リターダー段数を減らしてる
@@ -25,12 +26,12 @@ public class ReterderAllReduceAction : TelemetryActionBase
             input.Connect();
             input.SetRetarder(0);
             _reterderLevel = 0;
-            return true;
+            changed = true;
         }
         else
         {
             _reterderLevel = reterderLevel;
-            return false;
         }
+        return changed;
     }
 }
