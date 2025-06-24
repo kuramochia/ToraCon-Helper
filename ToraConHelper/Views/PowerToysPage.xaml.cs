9cd576096133034d7cc4dedc119bba10cdc561ba
kuramochia <kuramochia@live.jp>
2025-06-24 18:54:01 +0900

    現在時刻表示を Timer に変更

--- ToraConHelper/Views/PowerToysPage.xaml.cs
+++ ToraConHelper/Views/PowerToysPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Timers;
+using System.Windows.Controls;
+using System.Windows.Threading;
 using ToraConHelper.ViewModels;
 
 namespace ToraConHelper.Views;
@@ -8,9 +11,15 @@ namespace ToraConHelper.Views;
 /// </summary>
 public partial class PowerToysPage : Page
 {
+    private Timer timer = new(1000);
     public PowerToysPage(ViewModel viewModel)
     {
         InitializeComponent();
         DataContext = viewModel;
+        timer.Elapsed += (sender, e) =>
+        {
+           _ = Dispatcher.InvokeAsync(() => realTimeText.Text = DateTime.Now.ToString("HH:mm:ss"));
+        };
+        timer.Start();
     }
 }
