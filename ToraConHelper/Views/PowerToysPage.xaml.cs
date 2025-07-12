a738c6ef7dd15e78a66e132a4e6d4883c5e7f76d
kuramochia <kuramochia@live.jp>
2025-07-12 11:54:27 +0900

    ゲーム内情報が閉じているときでも現在時刻をヘッダに表示する

--- ToraConHelper/Views/PowerToysPage.xaml.cs
+++ ToraConHelper/Views/PowerToysPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Timers;
 using System.Windows.Controls;
 using System.Windows.Threading;
@@ -18,8 +19,26 @@ public partial class PowerToysPage : Page
         DataContext = viewModel;
         timer.Elapsed += (sender, e) =>
         {
-           _ = Dispatcher.InvokeAsync(() => realTimeText.Text = DateTime.Now.ToString("HH:mm:ss"));
+            _ = Dispatcher.InvokeAsync(() =>
+            {
+                var nowStr = DateTime.Now.ToString("HH:mm:ss");
+                realTimeText.Text = nowStr;
+                realTimeTitleText.Text = nowStr;
+            });
         };
         timer.Start();
     }
+
+    private void gameInfoExpander_ExpandedChanged(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if (gameInfoExpander.IsExpanded)
+        {
+            Dispatcher.InvokeAsync(() => realTimeTitleText.Visibility = Visibility.Collapsed);
+        }
+        else
+        {
+            Dispatcher.InvokeAsync(() => realTimeTitleText.Visibility = Visibility.Visible);
+        }
+    }
+
 }
