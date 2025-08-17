d9fcd0756d30abcbdc91589656e9078635e49609
kuramochia <kuramochia@live.jp>
2025-08-17 16:00:38 +0900

    アクセルを踏んでいる状態でリターダーを戻す入力を行うと、リターダーを一気に全段戻すアクションを追加

---
+++ ToraConHelper/Services/TelemetryActions/ReterderAllReduceOnThrottleAction.cs
@@ -0,0 +1,36 @@
+﻿using SCSSdkClient.Input;
+using SCSSdkClient.Object;
+
+namespace ToraConHelper.Services.TelemetryActions;
+
+public class ReterderAllReduceOnThrottleAction : TelemetryActionBase
+{
+    private const float LimitThrottle = 0.1f; // スロットルの閾値
+    private uint _reterderLevel = 0;
+
+    public override bool OnTelemetryUpdated(SCSTelemetry telemetry)
+    {
+        var changed = false;
+        var reterderLevel = telemetry.TruckValues.CurrentValues.MotorValues.BrakeValues.RetarderLevel;
+        var currentAccel = telemetry.ControlValues.InputValues.Throttle;
+
+        //Debug.WriteLine($"ReterderAllReduceOnThrottleAction: reterderLevel={reterderLevel}, currentAccel={currentAccel}");
+
+        // リターダー段数を減らしてる
+        // スロットルがONの場合
+        if (_reterderLevel > 0 && _reterderLevel > reterderLevel && currentAccel >= LimitThrottle)
+        {
+            // リターダー段数0を入力する
+            using var input = new SCSSdkTelemetryInput();
+            input.Connect();
+            input.SetRetarder(0);
+            _reterderLevel = 0;
+            changed = true;
+        }
+        else
+        {
+            _reterderLevel = reterderLevel;
+        }
+        return changed;
+    }
+}
