6ad0a145be84e3a7a34fe44b4873906e67c8ad6a
kuramochia <kuramochia@live.jp>
2025-10-10 20:55:21 +0900

    CCステップ1回未満の速度差の場合に、CCが増加しない問題の修正 (#103)

--- ToraConHelper/Services/TelemetryActions/FollowSpeedLimitCruiseControlAction.cs
+++ ToraConHelper/Services/TelemetryActions/FollowSpeedLimitCruiseControlAction.cs
@@ -9,11 +9,18 @@ public class FollowSpeedLimitCruiseControlAction : TelemetryActionBase
 {
     public int CruiseControlStep { get; set; } = 5;
     public bool MphInATS { get; set; } = true;
+
+    private bool ccEnabled = false;
     public override bool OnTelemetryUpdated(SCSTelemetry telemetry)
     {
         bool changed = false;
         if (telemetry.TruckValues.CurrentValues.DashboardValues.CruiseControl)
         {
+            // Off から On になった場合、初回として扱う
+            var firstTime = false;
+            if (!ccEnabled) firstTime = true;
+            ccEnabled = true;
+
             var useMph = MphInATS && telemetry.Game == SCSSdkClient.SCSGame.Ats;
 
             var currentLimit = Math.Floor(useMph ? telemetry.NavigationValues.SpeedLimit.Mph : telemetry.NavigationValues.SpeedLimit.Kph);
@@ -23,13 +30,13 @@ public class FollowSpeedLimitCruiseControlAction : TelemetryActionBase
 
             if (currentLimit > 0 && currentCc > 0)
             {
-                // 制限速度 > CC速度 + ステップ
-                if (currentLimit >= currentCc + CruiseControlStep)
+                // 制限速度 > CC速度 + ステップ か、初回で 制限速度 > CC速度 の場合、CC速度を上げる
+                if ((currentLimit >= currentCc + CruiseControlStep) || (firstTime && currentLimit > currentCc))
                 {
                     //CC速度UP
                     using var input = new SCSSdkTelemetryInput();
                     input.Connect();
-                    input.SetCruiseControlIncrement();
+                    input.SetCruiseControlIncrement(25);
                     changed = true;
                     Debug.WriteLine("CC Increment");
                 }
@@ -38,12 +45,17 @@ public class FollowSpeedLimitCruiseControlAction : TelemetryActionBase
                     //CC速度Down
                     using var input = new SCSSdkTelemetryInput();
                     input.Connect();
-                    input.SetCruiseControlDecrement();
+                    input.SetCruiseControlDecrement(25);
                     changed = true;
                     Debug.WriteLine("CC Decrement");
                 }
             }
         }
+        else
+        {
+            ccEnabled = false;
+        }
+
         return changed;
     }
 }
